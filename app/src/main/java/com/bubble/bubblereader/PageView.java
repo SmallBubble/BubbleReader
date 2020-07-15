42f6a9384a2bd1e131f2cf2c119677ab7f440529
weijiaxiang <1337986595@qq.com>
2020-07-15 17:55:32 +0800

    仿真翻页修改V1

--- app/src/main/java/com/bubble/bubblereader/PageView.java
+++ app/src/main/java/com/bubble/bubblereader/PageView.java
@@ -1,70 +1,84 @@
-package com.bubble.reader.widget.draw.impl;
+package com.bubble.bubblereader;
 
+import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PointF;
 import android.os.Build;
+import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.View;
 
-import com.bubble.reader.widget.PageView;
-import com.bubble.reader.widget.draw.base.PageDrawHelper;
+import androidx.annotation.Nullable;
 
 /**
  * @author Bubble
- * @date 2020/6/21
+ * @date 2020/7/15
  * @email 1337986595@qq.com
  * @GitHub https://github.com/SmallBubble
  * @Gitte https://gitee.com/SmallCatBubble
- * @Desc 仿真翻页
+ * @Desc TODO
  */
-public class SimulationDrawHelper extends PageDrawHelper {
-
-    private PointF mPointA = new PointF();
-    private PointF mPointB = new PointF();
-    private PointF mPointC = new PointF();
-    private PointF mPointD = new PointF();
-    private PointF mPointE = new PointF();
-    private PointF mPointF = new PointF();
-    private PointF mPointG = new PointF();
-    private PointF mPointH = new PointF();
-    private PointF mPointI = new PointF();
-    private PointF mPointJ = new PointF();
-    private PointF mPointK = new PointF();
-
-    private Path mPathFront;
-    private Path mPathBack;
-    private Path mPathNext;
+public class PageView extends View {
+    PointF mPointA = new PointF();
+    PointF mPointB = new PointF();
+    PointF mPointC = new PointF();
+    PointF mPointD = new PointF();
+    PointF mPointE = new PointF();
+    PointF mPointF = new PointF();
+    PointF mPointG = new PointF();
+    PointF mPointH = new PointF();
+    PointF mPointI = new PointF();
+    PointF mPointJ = new PointF();
+    PointF mPointK = new PointF();
+    Path mPathFront;
+    Path mPathBack;
+    Path mPathNext;
+    private int mWidth = 1080;
+    private int mHeight = 1920;
     private Paint mPaint;
 
-    public SimulationDrawHelper(PageView pageView) {
-        super(pageView);
+    public PageView(Context context) {
+        this(context, null);
+    }
+
+    public PageView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public PageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
     }
 
     @Override
-    protected void initData() {
-        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mPaint.setColor(Color.YELLOW);
-
-        mPathFront = new Path();
-        mPathBack = new Path();
-        mPathNext = new Path();
-        mPointA.set(680, 1420);
-        mPointF.set(mPageWidth, mPageHeight);
-        calcPoints();
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        getFrontAreaPath();
+        getBackAreaPath();
+        getNextAreaPath();
+        canvas.drawPath(mPathFront, mPaint);
+        mPaint.setColor(Color.GREEN);
+        canvas.drawPath(mPathBack, mPaint);
+        mPaint.setColor(Color.BLUE);
+        canvas.drawPath(mPathNext, mPaint);
+//        canvas.drawCircle(mPointI.x, mPointI.y, 30, mPaint);
+//        canvas.drawCircle(mPointD.x, mPointD.y, 30, mPaint);
     }
 
+
     @Override
-    public void onTouchEvent(PageView view, MotionEvent event) {
+    public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                if (event.getY() > mPageHeight / 2f) {
+                if (event.getY() > mHeight / 2) {
                     //右下角
-                    mPointF.set(mPageWidth, mPageHeight);
+                    mPointF.set(mWidth, mHeight);
                 } else {
                     // 右上角
-                    mPointF.set(mPageWidth, 0);
+                    mPointF.set(mWidth, 0);
                 }
                 mPointA.set(event.getX(), event.getY());
                 break;
@@ -77,33 +91,16 @@ public class SimulationDrawHelper extends PageDrawHelper {
             default:
         }
         calcPoints();
-        mPageView.invalidate();
+        invalidate();
+        return true;
     }
 
-    @Override
-    public void onDrawPage(Canvas canvas) {
-        getFrontAreaPath();
-        getBackAreaPath();
-        getNextAreaPath();
-
-        canvas.drawPath(mPathFront, mPaint);
-
-        mPaint.setColor(Color.GREEN);
-        canvas.drawPath(mPathBack, mPaint);
-
-        mPaint.setColor(Color.BLUE);
-        canvas.drawPath(mPathNext, mPaint);
-    }
-
-    /**
-     * 获取正面的显示区域
-     */
     private void getFrontAreaPath() {
         mPathFront.reset();
         if (mPointF.y == 0) {
             mPathFront.moveTo(0, 0);
-            mPathFront.lineTo(0, mPageHeight);
-            mPathFront.lineTo(mPageWidth, mPageHeight);
+            mPathFront.lineTo(0, mHeight);
+            mPathFront.lineTo(mWidth, mHeight);
             mPathFront.lineTo(mPointJ.x, mPointJ.y);
             mPathFront.quadTo(mPointH.x, mPointH.y, mPointK.x, mPointK.y);
             mPathFront.lineTo(mPointA.x, mPointA.y);
@@ -112,20 +109,17 @@ public class SimulationDrawHelper extends PageDrawHelper {
             mPathFront.close();
         } else {
             mPathFront.moveTo(0, 0);
-            mPathFront.lineTo(0, mPageHeight);
+            mPathFront.lineTo(0, mHeight);
             mPathFront.lineTo(mPointC.x, mPointC.y);
             mPathFront.quadTo(mPointE.x, mPointE.y, mPointB.x, mPointB.y);
             mPathFront.lineTo(mPointA.x, mPointA.y);
             mPathFront.lineTo(mPointK.x, mPointK.y);
             mPathFront.quadTo(mPointH.x, mPointH.y, mPointJ.x, mPointJ.y);
-            mPathFront.lineTo(mPageWidth, 0);
+            mPathFront.lineTo(mWidth, 0);
         }
         mPathFront.close();
     }
 
-    /**
-     * 获取背面的显示区域
-     */
     private void getBackAreaPath() {
         mPathBack.reset();
         mPathBack.moveTo(mPointD.x, mPointD.y);
@@ -146,33 +140,41 @@ public class SimulationDrawHelper extends PageDrawHelper {
         if (mPointC.x >= 0) {
             return;
         }
-
-
-
-
         int width1 = mPointF.y > 0 ? (int) (mPointF.x - mPointA.x) : (int) mPointA.x;
-        int width2 = mPointF.y > 0 ? (int) (mPageWidth - mPointC.x) : (int) (width1 - mPointC.x);
-        int height2 = mPointF.y > 0 ? (int) (mPageHeight - mPointA.y) : (int) mPointA.y;
+        int width2 = mPointF.y > 0 ? (int) (mWidth - mPointC.x) : (int) (width1 - mPointC.x);
+        int height2 = mPointF.y > 0 ? (int) (mHeight - mPointA.y) : (int) mPointA.y;
         int height1 = width1 * height2 / width2;
-        mPointA.set(mPageHeight - height1, mPageHeight - height1);
+        mPointA.set(mHeight - height1, mHeight - height1);
         calcPoints();
     }
 
-    /**
-     * 获取下一页的显示区域
-     */
     private void getNextAreaPath() {
         mPathNext.reset();
         mPathNext.moveTo(mPointC.x, mPointC.y);
         mPathNext.lineTo(mPointF.x, mPointF.y);
         mPathNext.lineTo(mPointJ.x, mPointJ.y);
         mPathNext.close();
-        Path tempPath = new Path();
+        Path path = new Path();
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            tempPath.op(mPathFront, Path.Op.UNION);
-            tempPath.op(mPathBack, Path.Op.UNION);
-            mPathNext.op(tempPath, Path.Op.DIFFERENCE);
+            path.op(mPathFront, Path.Op.UNION);
+            path.op(mPathBack, Path.Op.UNION);
+            mPathNext.op(path, Path.Op.DIFFERENCE);
         }
+    }
+
+    private void init() {
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(Color.YELLOW);
+
+        mPathFront = new Path();
+        mPathBack = new Path();
+        mPathNext = new Path();
+        mPointA.set(680, 1420);
+        mPointF.set(mWidth, mHeight);
+        calcPoints();
+
+
     }
 
     /**
@@ -207,22 +209,22 @@ public class SimulationDrawHelper extends PageDrawHelper {
     /**
      * 计算两线段相交点坐标
      *
-     * @param lineOnePointOne 线段1 的点1
-     * @param lineOnePointTwo 线段1 的点2
-     * @param lineTwoPointOne 线段2 的点1
-     * @param lineTwoPointTwo 线段2 的点2
+     * @param lineOneMyPointOne
+     * @param lineOneMyPointTwo
+     * @param lineTwoMyPointOne
+     * @param lineTwo_My_pointTwo
      * @return 返回该点
      */
-    private PointF getIntersectionPoint(PointF lineOnePointOne, PointF lineOnePointTwo, PointF lineTwoPointOne, PointF lineTwoPointTwo) {
+    private PointF getIntersectionPoint(PointF lineOneMyPointOne, PointF lineOneMyPointTwo, PointF lineTwoMyPointOne, PointF lineTwo_My_pointTwo) {
         float x1, y1, x2, y2, x3, y3, x4, y4;
-        x1 = lineOnePointOne.x;
-        y1 = lineOnePointOne.y;
-        x2 = lineOnePointTwo.x;
-        y2 = lineOnePointTwo.y;
-        x3 = lineTwoPointOne.x;
-        y3 = lineTwoPointOne.y;
-        x4 = lineTwoPointTwo.x;
-        y4 = lineTwoPointTwo.y;
+        x1 = lineOneMyPointOne.x;
+        y1 = lineOneMyPointOne.y;
+        x2 = lineOneMyPointTwo.x;
+        y2 = lineOneMyPointTwo.y;
+        x3 = lineTwoMyPointOne.x;
+        y3 = lineTwoMyPointOne.y;
+        x4 = lineTwo_My_pointTwo.x;
+        y4 = lineTwo_My_pointTwo.y;
         float pointX = ((x1 - x2) * (x3 * y4 - x4 * y3) - (x3 - x4) * (x1 * y2 - x2 * y1))
                 / ((x3 - x4) * (y1 - y2) - (x1 - x2) * (y3 - y4));
         float pointY = ((y1 - y2) * (x3 * y4 - x4 * y3) - (x1 * y2 - x2 * y1) * (y3 - y4))
